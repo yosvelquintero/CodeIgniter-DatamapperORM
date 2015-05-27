74765093b24fb3fb8c9066dd4ea0e170ec5ebdf5
Yosvel Quintero <yosvelquintero@gmail.com>
2015-05-27 19:39:14 +0200

    Update db.sql
    Updates on records... Credentials for admin user: admin/allcontrol

--- db.sql
+++ db.sql
@@ -48,14 +48,6 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
   KEY `last_activity_idx` (`last_activity`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dumping data for table ci.ci_sessions: ~3 rows (approximately)
-/*!40000 ALTER TABLE `ci_sessions` DISABLE KEYS */;
-INSERT INTO `ci_sessions` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
-	('3101ee6468552006fc1dc38d50adc125', '127.0.0.1', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:17.0) Gecko/20100101 Firefox/17.0', 1354824089, ''),
-	('66e7f5b3e70c49f0435648d3dccd907a', '127.0.0.1', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:17.0) Gecko/20100101 Firefox/17.0', 1355154400, ''),
-	('c5500d9b9182e97e6e0ea017ec98925a', '127.0.0.1', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:17.0) Gecko/20100101 Firefox/17.0', 1354809598, '');
-/*!40000 ALTER TABLE `ci_sessions` ENABLE KEYS */;
-
 
 -- Dumping structure for table ci.ci_users
 DROP TABLE IF EXISTS `ci_users`;
@@ -77,8 +69,8 @@ CREATE TABLE IF NOT EXISTS `ci_users` (
 -- Dumping data for table ci.ci_users: ~2 rows (approximately)
 /*!40000 ALTER TABLE `ci_users` DISABLE KEYS */;
 INSERT INTO `ci_users` (`id`, `group_id`, `name`, `username`, `email`, `password`, `salt`, `created`, `updated`) VALUES
-	(1, 1, 'demo name', 'user', 'user@gmail.com', '07d895a2ff5b819e754ca0f8c95b7e3cf6d875e3', '77828201b5c94f8e2872cdeae7b4f9c5', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-	(2, 2, 'Herman Placeres', 'herman', 'herman@gmail.com', '869b18ef8fb7b909a66b490c076279760a515946', '3d72cef3c0f6d0d17b153ad0d125a691', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+	(1, 1, 'Admin', 'admin', 'admin@ci.com', '07d895a2ff5b819e754ca0f8c95b7e3cf6d875e3', '77828201b5c94f8e2872cdeae7b4f9c5', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+	(2, 2, 'Manager', 'manager', 'manager@ci.com', '869b18ef8fb7b909a66b490c076279760a515946', '3d72cef3c0f6d0d17b153ad0d125a691', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 /*!40000 ALTER TABLE `ci_users` ENABLE KEYS */;
 /*!40014 SET FOREIGN_KEY_CHECKS=1 */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
